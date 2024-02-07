e9800228caa6c7e287a62799e35f0e24a8053185
Yoli-Buendia <bue17003@byui.edu>
2024-02-06 22:41:03 -0600

    https

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Users Api'
     },
     host: 'localhost:5000',
-    schemes: ['http', 'https']
+    schemes: ['https', 'http']
 };
 
 const outputFile = './swagger.json';
